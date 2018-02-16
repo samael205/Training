5b3a371208d6e429cf3a75ade724139f0c4bcebd
Rahul Madbhavi <rahulmadbhavi@gmail.com>
2018-02-16 11:56:24 +0530

    Completed chapter 16 Clone demo code.

---
+++ Java/Chapter_16/Clone/src/com/rahulmadbhavi/training/TestClone.java
@@ -0,0 +1,21 @@
+
+package com.rahulmadbhavi.training;
+
+class TestClone implements Cloneable
+{
+	int a;
+	double b;
+
+	TestClone cloneTest()
+	{
+		try
+		{
+			return (TestClone) super.clone();
+		}
+		catch(CloneNotSupportedException e)
+		{
+			System.out.println("Cloning not allowed.");
+			return this;
+		}
+	}
+}
