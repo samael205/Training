2d941b8d8a8d5eddff7ce54d18c3989d59955188
Rahul Madbhavi <rahulmadbhavi@gmail.com>
2018-02-16 12:49:13 +0530

    Complete chapter 16 Math code.

---
+++ Java/Chapter_16/Math/src/com/rahulmadbhavi/training/MathDemo.java
@@ -0,0 +1,88 @@
+
+package com.rahulmadbhavi.training;
+
+class MathDemo
+{
+	public static void main(String args[])
+	{
+		System.out.println();
+
+		System.out.println("Math.PI : " + Math.PI);
+		System.out.println("Math.E : " + Math.E);
+		System.out.println();
+
+		System.out.println("Math.sin(Math.PI/3) : " + Math.sin(Math.PI/3));
+		System.out.println("Math.cos(Math.PI/3) : " + Math.cos(Math.PI/3));
+		System.out.println("Math.tan(Math.PI/3) : " + Math.tan(Math.PI/3));
+		System.out.println();
+
+		System.out.println("Math.asin(1) : " + Math.asin(1));
+		System.out.println("Math.acos(1) : " + Math.acos(1));
+		System.out.println("Math.atan(1) : " + Math.atan(1));
+		System.out.println("Math.atan2(1, Math.sqrt(3)) : " + Math.atan2(1, Math.sqrt(3)));
+		System.out.println();
+
+		System.out.println("Math.sinh(1) : " + Math.sinh(1));
+		System.out.println("Math.cosh(1) : " + Math.cosh(1));
+		System.out.println("Math.tanh(1) : " + Math.tanh(1));
+		System.out.println();
+
+		System.out.println("Math.sqrt(6.25) : " + Math.sqrt(6.25));
+		System.out.println("Math.cbrt(15.625) : " + Math.cbrt(15.625));
+		System.out.println();
+
+		System.out.println("Math.exp(1) : " + Math.exp(1));
+		System.out.println("Math.log(2) : " + Math.log(2));
+		System.out.println("Math.log10(2) : " + Math.log10(2));
+		System.out.println();
+
+		System.out.println("Math.pow(2, 5) : " + Math.pow(2, 5));
+		System.out.println("Math.scalb(3, 4) : " + Math.scalb(3, 4));
+		System.out.println();
+
+		System.out.println("Math.abs(-1.25) : " + Math.abs(-1.25));
+		System.out.println("Math.abs(1.25) : " + Math.abs(1.25));
+		System.out.println();
+
+		System.out.println("Math.ceil(1.25) : " + Math.ceil(1.25));		
+		System.out.println("Math.floor(1.25) : " + Math.floor(1.25));		
+		System.out.println();
+
+		System.out.println("Math.max(1.25, 2.50) : " + Math.max(1.25, 2.50));
+		System.out.println("Math.min(1.25, 2.50) : " + Math.min(1.25, 2.50));
+		System.out.println();
+
+		System.out.println("Math.nextAfter(1.00, 2.00) : " + Math.nextAfter(1.00, 2.00));
+		System.out.println("Math.nextAfter(1.00F, 2.00) : " + Math.nextAfter(1.00F, 2.00));
+		System.out.println();
+
+		System.out.println("Math.nextUp(1.00) : " + Math.nextUp(1.00));
+		System.out.println("Math.nextUp(1.00F) : " + Math.nextUp(1.00F));
+		System.out.println();
+
+		System.out.println("Math.round(1.49) : " + Math.round(1.49));
+		System.out.println("Math.round(1.50) : " + Math.round(1.50));
+		System.out.println("Math.round(1.51) : " + Math.round(1.51));
+		System.out.println();
+
+		System.out.println("Math.round(1.49F) : " + Math.round(1.49F));
+		System.out.println("Math.round(1.50F) : " + Math.round(1.50F));
+		System.out.println("Math.round(1.51F) : " + Math.round(1.51F));
+		System.out.println();
+
+		System.out.println("Math.hypot(3.0, 4.0) : " + Math.hypot(3.0, 4.0));
+		System.out.println();
+
+		System.out.println("Math.random() : " + Math.random());
+		System.out.println();
+
+		System.out.println("Math.signum(-10.0) : " + Math.signum(-10.0));
+		System.out.println("Math.signum(0.0) : " + Math.signum(0.0));
+		System.out.println("Math.signum(10.0) : " + Math.signum(10.0));
+		System.out.println();
+
+		System.out.println("Math.toDegrees(Math.PI / 2) : " + Math.toDegrees(Math.PI / 2));
+		System.out.println("Math.toRadians(90) : " + Math.toRadians(90));
+		System.out.println();
+	}
+}
