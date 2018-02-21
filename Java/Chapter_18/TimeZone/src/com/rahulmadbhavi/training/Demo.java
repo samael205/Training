3e43d56bd872fc3b7bf49c21ff4529051dcb399a
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-02-22 00:04:02 +0530

    Completed chapter 18 TimeZone code.

---
+++ Java/Chapter_18/TimeZone/src/com/rahulmadbhavi/training/Demo.java
@@ -0,0 +1,14 @@
+
+package com.rahulmadbhavi.training;
+
+import java.util.*;
+
+class Demo
+{
+	public static void main(String args[])
+	{
+		TimeZone tz = new SimpleTimeZone(19800000, "IST");
+
+		System.out.println("tz : " + tz);
+	}
+}
