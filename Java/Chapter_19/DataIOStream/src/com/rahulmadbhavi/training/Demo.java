0dccad7f287405908c97484469c7a8e6838b6ef8
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-02-24 00:19:47 +0530

    Completed chapter 19 DataInputStream, DataOutputStream code.

---
+++ Java/Chapter_19/DataIOStream/src/com/rahulmadbhavi/training/Demo.java
@@ -0,0 +1,28 @@
+
+package com.rahulmadbhavi.training;
+
+import java.io.*;
+
+class Demo
+{
+	public static void main(String args[]) throws IOException
+	{
+		FileOutputStream fout = new FileOutputStream("Test.dat");
+		DataOutputStream out = new DataOutputStream(fout);
+
+		out.writeDouble(98.6);
+		out.writeInt(1000);
+		out.writeBoolean(true);
+
+		out.close();
+
+		FileInputStream fin = new FileInputStream("Test.dat");
+		DataInputStream in = new DataInputStream(fin);
+
+		double d = in.readDouble();
+		int i = in.readInt();
+		boolean b = in.readBoolean();
+
+		System.out.println("Here are the values : " + d + ", " + i + " and " + b);
+	}
+}
