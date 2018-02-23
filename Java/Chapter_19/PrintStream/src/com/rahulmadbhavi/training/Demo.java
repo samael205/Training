bbbca37d8879002f0c91a849039733eb712de32a
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-02-24 00:21:54 +0530

    Completed chapter 19 PrintStream code.

---
+++ Java/Chapter_19/PrintStream/src/com/rahulmadbhavi/training/Demo.java
@@ -0,0 +1,27 @@
+
+package com.rahulmadbhavi.training;
+
+import java.io.*;
+
+class Demo
+{
+	public static void main(String args[])
+	{
+		System.out.println("Here are some numeric values in different formats.\n");
+		System.out.println();
+
+		System.out.printf("Various integer formats : ");
+		System.out.printf("%d %(d %+d %05d\n", 3, -3, 3, 3);
+		System.out.println();
+
+		System.out.printf("Default floating-point format : %f\n", 1234567.123);
+		System.out.printf("Floating-point with commas : %,f\n", 1234567.123);
+		System.out.printf("Negative floating-point default : %,f\n", -1234567.123);
+		System.out.printf("Negative floating-point option : %,(f\n", -1234567.123);
+		System.out.println();
+
+		System.out.printf("Align positive and negative values:\n");
+		System.out.printf("% ,.2f\n% ,.2f\n", 1234567.123, -1234567.123);
+		System.out.println();
+	}
+}
