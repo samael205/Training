89d16fac75c9e05e06852dd84b3ae2b62a4598d4
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-03-02 00:50:58 +0530

    Completed chapter 24 GridLayout code.

---
+++ Java/Chapter_24/GridLayout/src/com/rahulmadbhavi/training/GridLayoutDemo.java
@@ -0,0 +1,31 @@
+package com.rahulmadbhavi.training;
+
+import java.applet.Applet;
+import java.awt.Button;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+public class GridLayoutDemo extends Applet
+{
+	static final int n = 4;
+	
+	public void init()
+	{
+		setLayout(new GridLayout(n, n));
+		
+		setFont(new Font("SansSerif", Font.BOLD, 24));
+		
+		for(int i=0; i<n; i++)
+		{
+			for(int j=0; j<n; j++)
+			{
+				int k = i*n + j;
+				
+				if(k > 0)
+				{
+					add(new Button("" + k));
+				}
+			}
+		}
+	}
+}
