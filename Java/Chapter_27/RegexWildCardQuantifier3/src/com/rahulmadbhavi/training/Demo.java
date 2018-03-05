85dafc6776a1aee5b9eb82bf893a77b6f4d7440c
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-03-05 08:38:09 +0530

    Completed chapter 27 RegexWildCardQuantifier3 code.

---
+++ Java/Chapter_27/RegexWildCardQuantifier3/src/com/rahulmadbhavi/training/Demo.java
@@ -0,0 +1,18 @@
+
+package com.rahulmadbhavi.training;
+
+import java.util.regex.*;
+
+public class Demo
+{
+	public static void main(String args[])
+	{
+		Pattern pattern = Pattern.compile("e.+?d");
+		Matcher matcher = pattern.matcher("extend cup end table");
+
+		while(matcher.find())
+		{
+			System.out.println("Match : " + matcher.group());
+		}
+	}
+}
