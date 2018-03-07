e2e1b66018416713ceaeb3a00b256052d7a4fb33
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-03-07 14:48:45 +0530

    Completed chapter 28 SimpleProperties code.

---
+++ Java/Chapter_28/SimpleProperties/src/com/rahulmadbhavi/training/Cuboid.java
@@ -0,0 +1,39 @@
+
+package com.rahulmadbhavi.training;
+
+public class Cuboid
+{
+	private double width;
+	private double breadth;
+	private double height;
+
+	public double getWidth()
+	{
+		return width;
+	}
+
+	public double getBreadth()
+	{
+		return breadth;
+	}
+
+	public double getHeight()
+	{
+		return height;
+	}
+
+	public void setWidth(double width)
+	{
+		this.width = width;
+	}
+
+	public void setBreadth(double breadth)
+	{
+		this.breadth = breadth;
+	}
+
+	public void setHeight(double height)
+	{
+		this.height = height;
+	}
+}
