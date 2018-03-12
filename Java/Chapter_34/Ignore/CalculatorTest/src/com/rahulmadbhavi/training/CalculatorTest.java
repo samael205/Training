dc3cebd123deded7ef7cc4d42184cb033ee4b1eb
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-03-12 07:40:38 +0530

    Completed chapter 34 Ignore Annotation code.

---
+++ Java/Chapter_34/Ignore/CalculatorTest/src/com/rahulmadbhavi/training/CalculatorTest.java
@@ -0,0 +1,42 @@
+package com.rahulmadbhavi.training;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class CalculatorTest
+{
+	public static Calculator calculator;
+	
+	@BeforeClass
+	public static void before()
+	{
+		System.out.println("Before All Tests");
+		calculator = new Calculator();
+	}
+	
+	@AfterClass
+	public static void after()
+	{
+		System.out.println("After All Tests");
+	}
+	
+	@Test
+	public void testAdd()
+	{
+		System.out.println("Testing add() function.");
+		assertEquals(5, calculator.add(2, 3));
+	}
+
+	@Ignore
+	@Test
+	public void testSub()
+	{
+		System.out.println("Testing sub() function.");
+		assertEquals(-1, calculator.sub(2, 3));
+	}
+
+}
