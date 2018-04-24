9cfa3bd8d05a365060452e59ddb3890c83397579
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-04-24 08:30:12 +0530

    Completed JavaFX Chapter 01 ToolTips code.

---
+++ JavaFX/Chapter_01/ToolTips/src/com/rahulmadbhavi/training/ToolTipsDemo.java
@@ -0,0 +1,53 @@
+package com.rahulmadbhavi.training;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+public class ToolTipsDemo extends Application
+{
+
+	public static void main(String[] args)
+	{
+		// Start the JavaFX Application by calling launch() method.
+		launch(args);
+	}
+	
+	// Override the start() method.
+	@Override
+	public void start(Stage primaryStage) throws Exception
+	{
+		// Give the stage a title.
+		primaryStage.setTitle("Tool Tips");
+		
+		// Use a FlowPane for the root node. In this case,
+		// vertical and horizontal gaps of 10 are used.
+		FlowPane flowPane = new FlowPane(10, 10);
+		
+		// Center the controls in the scene.
+		flowPane.setAlignment(Pos.CENTER);
+		
+		// Create a scene
+		Scene scene = new Scene(flowPane, 500, 100);
+		
+		// Set the scene on the stage.
+		primaryStage.setScene(scene);
+		
+		// Add a button
+		Button button = new Button("Button");
+		
+		// Set Tool Tip
+		button.setTooltip(new Tooltip("Click this button"));
+		
+		// Add button to scene.
+		flowPane.getChildren().add(button);
+		
+		// Show the stage and its scene.
+		primaryStage.show();
+	}
+
+}
