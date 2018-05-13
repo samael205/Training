623527e2bfe203456f3d36f76a89960eac5496d5
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-05-13 14:34:04 +0530

    Updated JavaScript Chapter 02 ComparisonOperators js code.

---
+++ JavaScript/Chapter_02/ComparisonOperators/js/script.js
@@ -0,0 +1,53 @@
+
+window.onload = function()
+{
+
+	document.getElementById('eq1').innerHTML = '4 == 4 : ' + (4 == 4);
+
+	document.getElementById('eq2').innerHTML = '4 == "4" : ' + (4 == "4");
+
+	document.getElementById('teq1').innerHTML = '4 === 4 : ' + (4 === 4);
+
+	document.getElementById('teq2').innerHTML = '4 === "4" : ' + (4 === "4");
+
+	document.getElementById('neq1').innerHTML = '4 != 2 : ' + (4 != 2);
+
+	document.getElementById('neq2').innerHTML = '4 != "4" : ' + (4 != "4");
+
+	document.getElementById('neq3').innerHTML = '4 != 4 : ' + (4 != 4);
+
+	document.getElementById('neqvt1').innerHTML = '4 !== 2 : ' + (4 !== 2);
+
+	document.getElementById('neqvt2').innerHTML = '4 !== "4" : ' + (4 !== "4");
+
+	document.getElementById('neqvt3').innerHTML = '4 !== 4 : ' + (4 !== 4);
+
+	document.getElementById('gt1').innerHTML = '4 > 2 : ' + (4 > 2);
+
+	document.getElementById('gt2').innerHTML = '4 > 4 : ' + (4 > 4);
+
+	document.getElementById('gt3').innerHTML = '4 > 6 : ' + (4 > 6);
+
+	document.getElementById('lt1').innerHTML = '4 < 2 : ' + (4 < 2);
+
+	document.getElementById('lt2').innerHTML = '4 < 4 : ' + (4 < 4);
+
+	document.getElementById('lt3').innerHTML = '4 < 6 : ' + (4 < 6);
+
+	document.getElementById('gte1').innerHTML = '4 >= 2 : ' + (4 >= 2);
+
+	document.getElementById('gte2').innerHTML = '4 >= 4 : ' + (4 >= 4);
+
+	document.getElementById('gte3').innerHTML = '4 >= 6 : ' + (4 >= 6);
+
+	document.getElementById('lte1').innerHTML = '4 <= 2 : ' + (4 <= 2);
+
+	document.getElementById('lte2').innerHTML = '4 <= 4 : ' + (4 <= 4);
+
+	document.getElementById('lte3').innerHTML = '4 <= 6 : ' + (4 <= 6);
+
+	document.getElementById('ter1').innerHTML = '4 > 2 ? "Yes" : "No" = ' + (4 > 2 ? "Yes" : "No");
+
+	document.getElementById('ter2').innerHTML = '4 < 2 ? "Yes" : "No" = ' + (4 < 2 ? "Yes" : "No");
+
+}
