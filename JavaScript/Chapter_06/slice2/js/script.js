b09340e244eff456db7fa1cfa8af8955ed9586c9
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-05-15 19:49:34 +0530

    Updated JavaScript Chapter 06 Slice2 js code.

---
+++ JavaScript/Chapter_06/slice2/js/script.js
@@ -0,0 +1,6 @@
+
+var string = "Red, Green, Blue";
+
+document.getElementById('output1').innerHTML = string.slice(-16, -13);
+document.getElementById('output2').innerHTML = string.slice(-11, -6);
+document.getElementById('output3').innerHTML = string.slice(-4);
