680fc3b250d5cb485d1cd45caaf6d4fbf89a8a75
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-05-15 19:49:20 +0530

    Updated JavaScript Chapter 06 Slice js code.

---
+++ JavaScript/Chapter_06/slice/js/script.js
@@ -0,0 +1,6 @@
+
+var string = "Red, Green, Blue";
+
+document.getElementById('output1').innerHTML = string.slice(0, 3);
+document.getElementById('output2').innerHTML = string.slice(5, 10);
+document.getElementById('output3').innerHTML = string.slice(12, 16);
