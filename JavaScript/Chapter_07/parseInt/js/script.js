832da2a175275e23b6d9d189ff11829ec6af05e4
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-05-16 21:39:09 +0530

    Updated JavaScript Chapter 07 ParseInt js code.

---
+++ JavaScript/Chapter_07/parseInt/js/script.js
@@ -0,0 +1,5 @@
+
+document.getElementById('output1').innerHTML = parseInt("10");
+document.getElementById('output2').innerHTML = parseInt(" 10");
+document.getElementById('output3').innerHTML = parseInt(" 10 20 test");
+document.getElementById('output4').innerHTML = parseInt("test 10");
