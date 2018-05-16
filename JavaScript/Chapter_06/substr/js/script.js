32cc7a54d8b1a56edea67094dde117cda768b23a
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-05-16 21:37:29 +0530

    Updated JavaScript Chapter 06 SubStr js code.

---
+++ JavaScript/Chapter_06/substr/js/script.js
@@ -0,0 +1,6 @@
+
+var string = "Red, Green, Blue";
+
+document.getElementById('output1').innerHTML = string.substr(0, 3);
+document.getElementById('output2').innerHTML = string.substr(5, 5);
+document.getElementById('output3').innerHTML = string.substr(12, 4);
