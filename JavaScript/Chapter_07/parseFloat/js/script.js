e41fe71611296e76ebc082dcb5b062146af63e83
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-05-16 21:38:55 +0530

    Updated JavaScript Chapter 07 ParseFloat js code.

---
+++ JavaScript/Chapter_07/parseFloat/js/script.js
@@ -0,0 +1,5 @@
+
+document.getElementById('output1').innerHTML = parseFloat("10");
+document.getElementById('output2').innerHTML = parseFloat(" 10f");
+document.getElementById('output3').innerHTML = parseFloat(" 10 20 test");
+document.getElementById('output4').innerHTML = parseFloat("test 10");
