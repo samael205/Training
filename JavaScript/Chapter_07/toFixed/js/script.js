45f2158bfc2aadb2d0d8c934a6157d44d861341e
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-05-17 22:21:05 +0530

    Updated JavaScript Chapter 07 ToFixed js code.

---
+++ JavaScript/Chapter_07/toFixed/js/script.js
@@ -0,0 +1,6 @@
+
+var number = 9.566;
+document.getElementById('output1').innerHTML = number.toFixed(0);
+document.getElementById('output2').innerHTML = number.toFixed(1);
+document.getElementById('output3').innerHTML = number.toFixed(2);
+document.getElementById('output4').innerHTML = number.toFixed(3);
