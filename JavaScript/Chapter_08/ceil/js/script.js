ca1994b43b83dae14e4bd93a5bd15fddfa05eee0
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-05-17 22:23:04 +0530

    Updated JavaScript Chapter 08 Ceil js code.

---
+++ JavaScript/Chapter_08/ceil/js/script.js
@@ -0,0 +1,4 @@
+
+document.getElementById('output1').innerHTML = Math.ceil(4);
+document.getElementById('output2').innerHTML = Math.ceil(4.5);
+document.getElementById('output3').innerHTML = Math.ceil(5);
