4624fb7be67cab24beceec0bdd173abda23f1eef
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-05-17 22:21:45 +0530

    Updated JavaScript Chapter 07 ToPrecesion js code.

---
+++ JavaScript/Chapter_07/toPrecision/js/script.js
@@ -0,0 +1,6 @@
+
+var number = 9.566;
+document.getElementById('output1').innerHTML = number.toPrecision();
+document.getElementById('output2').innerHTML = number.toPrecision(1);
+document.getElementById('output3').innerHTML = number.toPrecision(2);
+document.getElementById('output4').innerHTML = number.toPrecision(3);
