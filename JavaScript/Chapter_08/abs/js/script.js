83a5822170d50d3d03d64ab41985db94710a32c3
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-05-17 22:22:29 +0530

    Updated JavaScript Chapter 08 Abs js code.

---
+++ JavaScript/Chapter_08/abs/js/script.js
@@ -0,0 +1,4 @@
+
+document.getElementById('output1').innerHTML = Math.abs(4);
+document.getElementById('output2').innerHTML = Math.abs(0);
+document.getElementById('output3').innerHTML = Math.abs(-4);
