534dd1c64f238fb02b067a5eedb0de1ce006310c
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-05-17 22:23:27 +0530

    Updated JavaScript Chapter 08 Constants js code.

---
+++ JavaScript/Chapter_08/constants/js/script.js
@@ -0,0 +1,9 @@
+
+document.getElementById('output1').innerHTML = Math.PI;
+document.getElementById('output2').innerHTML = Math.E;
+document.getElementById('output3').innerHTML = Math.SQRT2;
+document.getElementById('output4').innerHTML = Math.SQRT1_2;
+document.getElementById('output5').innerHTML = Math.LN2;
+document.getElementById('output6').innerHTML = Math.LN10;
+document.getElementById('output7').innerHTML = Math.LOG2E;
+document.getElementById('output8').innerHTML = Math.LOG10E;
