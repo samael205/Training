7899e129a6006d911b002a916e3d1627ffd7ccd6
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-05-18 01:01:47 +0530

    Updated JavaScript Chapter 08 Trig js code.

---
+++ JavaScript/Chapter_08/trig/js/script.js
@@ -0,0 +1,4 @@
+
+document.getElementById('output1').innerHTML = Math.sin(Math.PI/4);
+document.getElementById('output2').innerHTML = Math.cos(Math.PI/4);
+document.getElementById('output3').innerHTML = Math.tan(Math.PI/4);
