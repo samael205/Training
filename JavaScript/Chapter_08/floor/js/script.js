8a0d781b6c8b1b544a073429122a727989a8ac19
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-05-17 22:23:43 +0530

    Updated JavaScript Chapter 08 Floor js code.

---
+++ JavaScript/Chapter_08/floor/js/script.js
@@ -0,0 +1,4 @@
+
+document.getElementById('output1').innerHTML = Math.floor(4);
+document.getElementById('output2').innerHTML = Math.floor(4.5);
+document.getElementById('output3').innerHTML = Math.floor(5);
