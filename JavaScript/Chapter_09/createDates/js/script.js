b2366ae50969057826a33e603c0e06e933139189
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-05-19 16:50:58 +0530

    Updated JavaScript Chapter 09 CreateDates js code.

---
+++ JavaScript/Chapter_09/createDates/js/script.js
@@ -0,0 +1,5 @@
+
+document.getElementById('output1').innerHTML = new Date();
+document.getElementById('output2').innerHTML = new Date(1521026265673);
+document.getElementById('output3').innerHTML = new Date("03-14-2018 16:30:05.123");
+document.getElementById('output4').innerHTML = new Date(2018, 3, 14, 16, 30, 30, 534);
