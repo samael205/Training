176c93c5ffb693073a1dc3b7b5f9df04f5ce9c35
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-05-19 16:51:31 +0530

    Updated JavaScript Chapter 09 DateMethods js code.

---
+++ JavaScript/Chapter_09/dateMethods/js/script.js
@@ -0,0 +1,15 @@
+
+date = new Date();
+
+document.getElementById('output1').innerHTML = date.toString();
+document.getElementById('output2').innerHTML = date.toUTCString();
+document.getElementById('output3').innerHTML = date.toDateString();
+document.getElementById('output4').innerHTML = date.getDate();
+document.getElementById('output5').innerHTML = date.getDay();
+document.getElementById('output6').innerHTML = date.getFullYear();
+document.getElementById('output7').innerHTML = date.getHours();
+document.getElementById('output8').innerHTML = date.getMilliseconds();
+document.getElementById('output9').innerHTML = date.getMinutes();
+document.getElementById('output10').innerHTML = date.getMonth();
+document.getElementById('output11').innerHTML = date.getSeconds();
+document.getElementById('output12').innerHTML = date.getTime();
