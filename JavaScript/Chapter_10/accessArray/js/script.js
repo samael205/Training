7be2b424fa9d4ab54d2a43f87edbd961096b4e10
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-05-19 16:51:53 +0530

    Updated JavaScript Chapter 10 AccessArray js code.

---
+++ JavaScript/Chapter_10/accessArray/js/script.js
@@ -0,0 +1,6 @@
+
+array = ["red", "green", "blue"];
+
+document.getElementById('output1').innerHTML = array[0];
+document.getElementById('output2').innerHTML = array[1];
+document.getElementById('output3').innerHTML = array[2];
