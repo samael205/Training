e55676ea0a8e4b7bc94a851a236192b5b7153752
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-05-25 21:10:35 +0530

    Updated JavaScript Chapter 18 Object js code.

---
+++ JavaScript/Chapter_18/object/js/script.js
@@ -0,0 +1,7 @@
+
+var sportsPerson = {firstName: "Roger", lastName: "Federer", country: "Switzerland", sport: "Tennis"};
+
+document.getElementById('firstName').innerHTML = sportsPerson.firstName;
+document.getElementById('lastName').innerHTML = sportsPerson.lastName;
+document.getElementById('country').innerHTML = sportsPerson.country;
+document.getElementById('sport').innerHTML = sportsPerson.sport;
