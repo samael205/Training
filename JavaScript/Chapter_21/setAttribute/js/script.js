d9559f0c4f7e41ec10c18e3ac13188e5e3a0eea5
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-06-01 10:35:44 +0530

    Updated JavaScript Chapter 21 SetAttribute js code.

---
+++ JavaScript/Chapter_21/setAttribute/js/script.js
@@ -0,0 +1,8 @@
+
+var nameField = document.getElementById('name');
+
+nameField.setAttribute('disabled', 'disabled');
+
+nameField.value = 'This field was disabled using JavaScript';			
+
+nameField.style.width = 300;
