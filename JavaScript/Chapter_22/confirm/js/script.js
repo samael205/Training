8d1d3b76aa34abf16cb6fc4dd8c8adcd18ecb716
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-06-03 21:20:22 +0530

    Updated JavaScript Chapter 22 Confirm js code.

---
+++ JavaScript/Chapter_22/confirm/js/script.js
@@ -0,0 +1,12 @@
+
+window.onload = function()
+{
+	if(confirm("Uploading your data to the internet"))
+	{
+		document.write("Data uploaded.");
+	}
+	else
+	{
+		document.write("Sorry, Data was already uploaded.");
+	}
+}
