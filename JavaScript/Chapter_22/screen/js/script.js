0c4acf32804a3d031c8507edb13e7572f09c8d9b
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-06-05 08:45:19 +0530

    Updated JavaScript Chapter 22 Screen js code.

---
+++ JavaScript/Chapter_22/screen/js/script.js
@@ -0,0 +1,10 @@
+
+window.onload = function()
+{
+	document.getElementById('screenWidth').innerHTML = screen.width;
+	document.getElementById('screenHeight').innerHTML = screen.height;
+	document.getElementById('screenAvailWidth').innerHTML = screen.availWidth;
+	document.getElementById('screenAvailHeight').innerHTML = screen.availHeight;
+	document.getElementById('screenColorDepth').innerHTML = screen.colorDepth;
+	document.getElementById('screenPixelDepth').innerHTML = screen.pixelDepth;
+}
