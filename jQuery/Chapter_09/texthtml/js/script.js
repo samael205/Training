e031a29b904761058b28748fa8f0517436aaa7d0
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-06-08 09:11:17 +0530

    Updated JQuery Chapter 09 TextHtml js code.

---
+++ jQuery/Chapter_09/texthtml/js/script.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function(){
+
+	$('#text').click(function(){
+
+		alert($('#content').text());
+
+	});
+
+	$('#html').click(function(){
+
+		alert($('#content').html());
+
+	});
+
+});
