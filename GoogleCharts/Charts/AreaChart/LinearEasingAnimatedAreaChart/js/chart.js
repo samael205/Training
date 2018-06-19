dd5ac773d5ddbb79c1269e80b1e2341f555a2f85
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-06-19 22:37:23 +0530

    Completed GoogleCharts AreaChart LinearEasingAnimatedAreaChart js code.

---
+++ GoogleCharts/Charts/AreaChart/LinearEasingAnimatedAreaChart/js/chart.js
@@ -0,0 +1,44 @@
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart()
+{
+	var data = google.visualization.arrayToDataTable([
+
+		['Year', 'Player A', 'Player B', 'Player C'],
+		['2013', 20, 7, 12],
+		['2014', 25, 15, 11],
+		['2015', 26, 12, 10],
+		['2016', 19, 28, 14],
+		['2017', 21, 26, 18]
+
+	]);
+
+	var options = 
+	{
+		title: 'Team Goal Scoreres',
+		hAxis: 
+				{
+					title: 'Year',
+					titleTextStyle:
+									{
+										color: '#333'
+									}
+				},
+		vAxis:
+				{
+					minValue: 0
+				},
+		animation:
+				{
+					startup: true,
+					duration: 500,
+					easing: 'linear'
+				}
+	};
+
+	var chart = new google.visualization.AreaChart(document.getElementById('chartDiv'));
+
+	chart.draw(data, options);
+}
