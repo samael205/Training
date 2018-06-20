6dc58b46c07c84c17c8ecf89956d6a313bab214b
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-06-20 22:27:10 +0530

    Completed GoogleCharts AreaChart VAxisTitleAreaChart js code.

---
+++ GoogleCharts/Charts/AreaChart/VAxisTitleAreaChart/js/chart.js
@@ -0,0 +1,50 @@
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart()
+{
+	var data = google.visualization.arrayToDataTable([
+
+		['Year', 'Player A', 'Player B', 'Player C'],
+		['2013', 20, 7, 12],
+		['2014', 25, 15, 11],
+		['2015', 26, 12, 10],
+		['2016', 19, 28, 14],
+		['2017', 21, 26, 18]
+
+	]);
+
+	var options = 
+	{
+		title: 'Team Goal Scoreres',
+		hAxis: 
+				{
+					title: 'Year',
+				},
+		vAxis:
+				{
+					minValue: 0,
+					titleTextStyle:
+									{
+										color: 'red',
+										fontName: 'Calibri',
+										fontSize: 15,
+										bold: true,
+										italic: false
+									},
+					textStyle:
+									{
+										color: 'darkblue',
+										fontName: 'Calibri',
+										fontSize: 15,
+										bold: true,
+										italic: false
+									}
+				}
+	};
+
+	var chart = new google.visualization.AreaChart(document.getElementById('chartDiv'));
+
+	chart.draw(data, options);
+}
