affec1d5c20b65cea6fe5364fed7d2bd019ae437
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-06-21 21:33:43 +0530

    Completed GoogleCharts BarChart BarChart js code.

---
+++ GoogleCharts/Charts/BarChart/BarChart/js/chart.js
@@ -0,0 +1,35 @@
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart()
+{
+	var data = google.visualization.arrayToDataTable([
+
+		['Player A', 'Runs'],
+		['Match 1', 25],
+		['Match 2', 15],
+		['Match 3', 35],
+		['Match 4', 10],
+		['Match 5', 12],
+		['Match 6', 27],
+		['Match 7', 95],
+		['Match 8', 18],
+		['Match 9', 45],
+		['Match 10', 52]
+
+	]);
+
+	var options = 
+	{
+		title: 'Player A Runs History',
+		width: 600,
+		height: 400,
+		bar: {groupWidth: '95%'},
+		legend: {position: 'none'}
+	};
+
+	var chart = new google.visualization.BarChart(document.getElementById('chartDiv'));
+
+	chart.draw(data, options);
+}
