d10d0fd796c8e1f64269ea6625066cec65d9e933
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-06-27 21:50:31 +0530

    Completed GoogleCharts CalendarChart FocusedCellColor js code.

---
+++ GoogleCharts/Charts/CalendarChart/FocusedCellColor/js/chart.js
@@ -0,0 +1,55 @@
+
+google.charts.load('current', {packages:['calendar']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart()
+{
+	var dataTable = new google.visualization.DataTable();
+
+	dataTable.addColumn({type: 'date', id:'Date'});
+	dataTable.addColumn({type: 'number', id:'Attendance'});
+
+	dataTable.addRows([
+
+		[new Date(2012, 3, 13), 37032],
+		[new Date(2012, 3, 14), 24673],
+		[new Date(2012, 3, 15), 32782],
+		[new Date(2012, 3, 16), 17847],
+		[new Date(2012, 3, 17), 34787],
+
+		[new Date(2013, 9, 4), 15232],
+		[new Date(2013, 9, 5), 38255],
+		[new Date(2013, 9, 12), 12498],
+		[new Date(2013, 9, 13), 26983],
+		[new Date(2013, 9, 19), 21589],
+		[new Date(2013, 9, 23), 25893],
+		[new Date(2013, 9, 24), 31523],
+		[new Date(2013, 9, 30), 38232]
+
+	]);
+
+	var chart = new google.visualization.Calendar(document.getElementById('chartDiv'));
+
+	var options = 
+	{
+		title: 'Stadium Attendance',
+		height: 350,
+		calendar:
+				{
+					cellColor:
+							{
+								stroke: '#76a7fa',
+								strokeOpacity: 0.5,
+								strokeWidth: 1
+							},
+					focusedCellColor:
+							{
+								stroke: '#d3362d',
+								strokeOpacity: 1,
+								strokeWidth: 1
+							}
+				}
+	};
+
+	chart.draw(dataTable, options);
+}
