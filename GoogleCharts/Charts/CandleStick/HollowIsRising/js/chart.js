85cb0c60d632d18bf9c2b0ca66e51b11619b2195
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-07-01 23:52:53 +0530

    Completed GoogleCharts CandleStick HollowIsRising js code.

---
+++ GoogleCharts/Charts/CandleStick/HollowIsRising/js/chart.js
@@ -0,0 +1,30 @@
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart()
+{
+	var data = google.visualization.arrayToDataTable([
+
+		['Mon', 20, 28, 38, 45],
+		['Tue', 31, 38, 55, 66],
+		['Wed', 50, 55, 77, 80],
+		['Thu', 77, 77, 66, 50],
+		['Fri', 68, 66, 22, 15]
+
+		// Treat first row as data.
+	], true);
+
+	var options =
+	{
+		legend: 'none',
+		candlestick:
+		{
+			hollowIsRising: true
+		}
+	};
+
+	var chart = new google.visualization.CandlestickChart(document.getElementById('chartDiv'));
+
+	chart.draw(data, options);
+}		
