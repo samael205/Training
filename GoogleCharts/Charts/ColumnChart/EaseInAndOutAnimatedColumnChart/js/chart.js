6816579ecdb38658af181ddac3b27d5f96e6e77b
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-07-02 11:46:45 +0530

    Completed GoogleCharts ColumnChart EaseInAndOut js code.

---
+++ GoogleCharts/Charts/ColumnChart/EaseInAndOutAnimatedColumnChart/js/chart.js
@@ -0,0 +1,32 @@
+
+google.charts.load('current', {packages: ['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart()
+{
+	var data = new google.visualization.arrayToDataTable([
+
+		['Year', 'Sales', 'Expenses', 'Profit'],
+		['2014', 1000, 400, 200],
+		['2015', 1170, 460, 250],
+		['2016', 660, 1120, 300],
+		['2017', 1030, 540, 350]
+
+	]);
+
+	var options =
+	{
+		title: 'Company Performance',
+		subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+		animation:
+		{
+			startup: true,
+			duration: 1000,
+			easing: 'inAndOut'
+		}
+	};
+
+	var chart = new google.visualization.ColumnChart(document.getElementById('chartDiv'));
+
+	chart.draw(data, options);
+}
