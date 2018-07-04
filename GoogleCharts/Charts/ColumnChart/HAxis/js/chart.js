cd8e9a85566b347e6fa313e2b8f4bf4359958aed
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-07-04 09:23:02 +0530

    Completed GoogleCharts ColumnChart HAxis js code.

---
+++ GoogleCharts/Charts/ColumnChart/HAxis/js/chart.js
@@ -0,0 +1,49 @@
+
+google.charts.load('current', {packages: ['bar']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart()
+{
+	var data = new google.visualization.arrayToDataTable([
+
+		['Year', 'Sales', 'Expenses', 'Profit'],
+		['2014', 1000, 400, 200],
+		['2015', 1170, 460, 250],
+		['2016', 660, 1120, 300],
+		['2017', 1030, 540, 350]
+
+	]);
+
+	var options =
+	{
+		chart:
+		{
+			title: 'Company Performance',
+			subtitle: 'Sales, Expenses, and Profit: 2014-2017'
+		},		
+		hAxis:
+		{
+			title: 'Year',
+			titleTextStyle:
+			{
+				color: 'darkred',
+				fontName: 'Calibri',
+				fontSize: 16,
+				bold: true,
+				italic: false
+			},
+			textStyle:
+			{
+				color: 'darkblue',
+				fontName: 'Calibri',
+				fontSize: 16,
+				bold: true,
+				italic: false
+			},
+		}
+	};
+
+	var chart = new google.charts.Bar(document.getElementById('chartDiv'));
+
+	chart.draw(data, google.charts.Bar.convertOptions(options));
+}
