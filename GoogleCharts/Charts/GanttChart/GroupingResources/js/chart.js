dd463e498637c29a404bc17dea3a9195bb281b81
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-07-10 21:14:40 +0530

    Completed GoogleCharts GanttChart GroupingResources js code.

---
+++ GoogleCharts/Charts/GanttChart/GroupingResources/js/chart.js
@@ -0,0 +1,40 @@
+
+google.charts.load("current", {packages: ["gantt"]});
+google.charts.setOnLoadCallback(drawChart);
+
+function daysToMilliseconds(days)
+{
+	return (days * 24 * 60 * 60 * 1000);
+}
+
+function drawChart()
+{
+	var data = new google.visualization.DataTable();
+
+	data.addColumn("string", "Task ID");
+	data.addColumn("string", "Task Name");
+	data.addColumn("string", "Resource");
+	data.addColumn("date", "Start Date");
+	data.addColumn("date", "End Date");
+	data.addColumn("number", "Duration");
+	data.addColumn("number", "Percent Complete");
+	data.addColumn("string", "Dependencies");
+
+	data.addRows([
+
+		["Research", "Find Sources", null, new Date(2015, 0, 1), new Date(2015, 0, 5), null, 100, null],
+		["Write", "Write Paper", "write", null, new Date(2015, 0, 9), daysToMilliseconds(3), 25, "Research,Outline"],
+		["Cite", "Create Bibliography", "write", null, new Date(2015, 0, 7), daysToMilliseconds(1), 20, "Research"],
+		["Complete", "Hand in Paper", "complete", null, new Date(2015, 0, 10), daysToMilliseconds(1), 0, "Cite,Write"],
+		["Outline", "Outline Paper", "write", null, new Date(2015, 0, 6), daysToMilliseconds(1), 100, "Research"]
+
+	]);
+
+	var options =
+	{
+
+	}
+
+	var chart = new google.visualization.Gantt(document.getElementById("chartDiv"));
+	chart.draw(data, options);
+}
