3692487945a032d207ab2143bbd4736128bda82b
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-07-13 17:48:22 +0530

    Completed GoogleCharts GeoChart GeoChart js code.

---
+++ GoogleCharts/Charts/GeoChart/GeoChart/js/chart.js
@@ -0,0 +1,33 @@
+
+google.charts.load("current", 	{
+									"packages": ["geochart"],
+									"mapsApiKey": "AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY"
+								});
+
+google.charts.setOnLoadCallback(drawRegionsMap);
+
+function drawRegionsMap()
+{
+
+	var data = google.visualization.arrayToDataTable([
+
+		["Country", "Popularity"],
+		["Germany", 200],
+		["United States", 300],
+		["Brazil", 400],
+		["Cannada", 500],
+		["France", 600],
+		["RU", 700]
+
+	]);
+
+	var options =
+	{
+
+	};
+
+	var chart = new google.visualization.GeoChart(document.getElementById("chartDiv"));
+
+	chart.draw(data, options);
+
+}
