5180e62736ed8c560199f46b74dac217e2646e28
Rahul-Madbhavi <rahulmadbhavi@gmail.com>
2018-07-14 12:19:51 +0530

    Completed GoogleCharts GeoChart Resolution js code.

---
+++ GoogleCharts/Charts/GeoChart/Resolution/js/chart.js
@@ -0,0 +1,35 @@
+
+google.charts.load("current", 	{
+									"packages": ["geochart"],
+									"mapsApiKey": "AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY"
+								});
+
+google.charts.setOnLoadCallback(drawMarkersMap);
+
+function drawMarkersMap()
+{
+
+	var data = google.visualization.arrayToDataTable([
+
+		["City", "Population", "Area"],
+		["Bangalore", 12300000, 709],
+		["Delhi", 19000000, 1484],
+		["Mumbai", 18400000, 603.4],
+		["Kolkata", 4500000, 205]
+
+	]);
+
+	var options =
+	{
+		region: "IN",
+		domain: "IN",
+		resolution: "provinces",
+		displayMode: "markers",
+		colorAxis: {colors: ["green", "blue"]}
+	};
+
+	var chart = new google.visualization.GeoChart(document.getElementById("chartDiv"));
+
+	chart.draw(data, options);
+
+}
